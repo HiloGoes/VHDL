6bca2ea437134788d9d85f7cdd802358eeccb974
Kaike Castro <42541528+kaikecc@users.noreply.github.com>
2019-06-09 16:03:53 -0300

    Update mde_semafaro.vhd

--- reg_semafaro/mde_semafaro.vhd
+++ reg_semafaro/mde_semafaro.vhd
@@ -3,14 +3,14 @@ use ieee . std_logic_1164 .all;
 
 entity mde_b is
 port ( clk , r , w: in std_logic ;
---z : out std_logic 
+--
 vd_C, am_C, vm_C, vd_M, am_M, vm_M: out std_logic);
 
 end mde_b;
 
 architecture ckt of mde_b is
                     -- a, b, c 
-type state_type is (espera, vd_vm , am_vm , vm_vd, vm_am);
+type state_type is (espera, am_inter, vd_vm , am_vm , vm_vd, vm_am);
 
 signal y_present , y_next : state_type ;
 begin
@@ -20,8 +20,12 @@ begin
 case y_present is
 
 when espera =>
-if w = '0' then y_next <= espera;
+if w = '0' then y_next <= am_inter;
 else y_next <= vd_vm; end if;
+
+when am_inter =>
+if w = '0' then y_next <= am_inter;
+else y_next <= espera; end if;
 
 when vd_vm =>
 if w = '0' then y_next <= vd_vm;
@@ -56,12 +60,20 @@ end process ;
 
 
 vd_C <= '0';
-am_C <= '0';
+am_C <= '1';
 vm_C <= '0';
 
 vd_M <= '0';
-am_M <= '0';
+am_M <= '1';
 vm_M <= '0' when y_present = espera;
+
+vd_C <= '0';
+am_C <= '1';
+vm_C <= '0';
+
+vd_M <= '0';
+am_M <= '1';
+vm_M <= '0' when y_present = am_inter;
 
 vd_C <= '1';
 am_C <= '0';
